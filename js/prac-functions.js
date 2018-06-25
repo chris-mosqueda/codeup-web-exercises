81393275e9d462a84ae2cea1eb8f28915e22c5ae
Natalie Mosqueda <natalie_bustos18@Natalies-MacBook-Pro-5.local>
2018-06-25 09:19:58 -0500

    bonuses

--- js/prac-functions.js
+++ js/prac-functions.js
@@ -20,11 +20,11 @@
     console.log(addThree(5));
 // 4) Make a function called sayString() which returns the string input passed in.
 // ---Test this function with console.log(sayString())
-//     function sayString(string){
-//         string = prompt("Enter in a word or words.")
-//         return string;
-//     }
-//     console.log(sayString());
+    function sayString(string){
+        // string = prompt("Enter in a word or words.")
+        // return string;
+    }
+    //console.log(sayString());
 // 5) Make a function called sayHowdy() which console.logs the string “Howdy!”
 // ---Test this function by directly calling sayHowdy()
     function sayHowdy(){
@@ -59,15 +59,72 @@ function last(input){
 console.log(last("jenny"));
 
 // Write a function called `rest(input)` that returns everything but the first character of a string.
+function rest(input) {
+    return input.substr(1);
+}
+console.log(rest("hello"));
+
 // Write a function called `reverse(input)` that takes a string and returns it reversed.
+function reverse(rev) {
+    return rev.split("").reverse().join("");
+}
+console.log(reverse("Reversey"));
+
 // Write a function called `isNumeric(input)` that takes an input and returns a boolean if the input is numeric.
+function isNumeric(numIn){
+    return !isNaN(parseInt(numIn));
+}
+console.log(isNumeric("notanum"));
+
 // Write a function called `count(input)` that takes in a string and returns the number of characters.
+function count(numString) {
+    return numString.length;
+}
+console.log(count("qwertyuiop"));
 // Write a function called `add(a, b)` that returns the sum of a and b
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2,6));
+
 // Write a function called `subtract(a, b)` that return the difference between the two inputs.
+function subtract(b, c) {
+    return b - c;
+}
+console.log(subtract(2,6));
+
 // Write `multiply(a, b)` function that returns the product
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(2,6));
+
 // Write a divide(numerator, denominator) function that returns a divided by b
+function divide(b, c) {
+    return b / c;
+}
+console.log(divide(6,2));
+
 // Write a remainder(number, divisor) function that returns the remainder left over when dividing `number` by the `divisor`
+function remainder(number, divisor) {
+    return number % divisor;
+}
+console.log(remainder(6,2));
+
 // Write the function `square(a)` that takes in a number and returns the number multiplied by itself.
+function square(a) {
+    return a * a;
+}
+console.log(square(6));
+
 // # Super Duper Gold-Star Bonus
 // Write a function called sumOfSquares(a, b) that uses only your add() function and your square function and not + or * operators
+function sumOfSquares(a, b) {
+    return add(square(a), square(b));
+}
+console.log(sumOfSquares(3, 5));
+
 // Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter is the name of the math function you want to apply. a and b are the two numbers to run that function on.
+
+//?????????????????
+
